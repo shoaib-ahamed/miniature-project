2542cd61a932f099b8be817fffbcecb3d9eb513b
shoaibnesoi <shoaib@nesoi.one>
2023-01-18 22:22:32 +0600

    add bottom-bar

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const BottomBar = () => {
   return (
-    <div className="h-16 bg-mainColor text-white flex gap-3 justify-center font-bold sticky top-[780px] z-10 md:hidden">
+    <div className="h-16 w-full bg-mainColor text-white gap-3 grid grid-cols-4 justify-center items-center font-bold fixed bottom-0 z-10 md:hidden">
         <div className="flex justify-center items-center">
             <button className="bg-mainColor border border-white hover:bg-slate-300 hover:text-mainColor h-10 w-20 rounded-md">
                 <Link href="/categories/mens-collection" >Mens</Link>
@@ -11,19 +11,19 @@ const BottomBar = () => {
 
         <div className="flex justify-center items-center">
             <button className="bg-mainColor border border-white hover:bg-slate-300 hover:text-mainColor h-10 w-20 rounded-md">
-                <Link href="/categories/jewelery">jewelery</Link>
+                <Link href="/categories/jewelery">Jewelery</Link>
             </button>
         </div>
 
         <div className="flex justify-center items-center">
             <button className="bg-mainColor border border-white hover:bg-slate-300 hover:text-mainColor h-10 w-20 rounded-md">
-                <Link href="/categories/electronics">gadget</Link>
+                <Link href="/categories/electronics">Gadget</Link>
             </button>
         </div>
 
         <div className="flex justify-center items-center">
             <button className="bg-mainColor border border-white hover:bg-slate-300 hover:text-mainColor h-10 w-20 rounded-md">
-            <Link href="/categories/womens-collection">womens</Link>
+            <Link href="/categories/womens-collection">Womens</Link>
             </button>
         </div>
     </div>
