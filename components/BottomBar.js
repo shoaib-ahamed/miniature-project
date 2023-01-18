f4ecaf461dd0b9fdbc38e43d2f2d4d9ef144d220
shoaibnesoi <shoaib@nesoi.one>
2023-01-18 22:06:16 +0600

    add bottom-bar

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const BottomBar = () => {
   return (
-    <div className="h-16 bg-mainColor text-white flex gap-3 justify-center font-bold sticky top-[780px] z-10">
+    <div className="h-16 bg-mainColor text-white flex gap-3 justify-center font-bold sticky top-[780px] z-10 md:hidden">
         <div className="flex justify-center items-center">
             <button className="bg-mainColor border border-white hover:bg-slate-300 hover:text-mainColor h-10 w-20 rounded-md">
                 <Link href="/categories/mens-collection" >Mens</Link>
